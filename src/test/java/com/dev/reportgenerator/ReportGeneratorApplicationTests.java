8a8b427ce0c6b2cc6b0cbc729d1c4c17364a9acf
Kien_Nguyen1 <kien_nguyen1@epam.com>
2022-12-06 23:09:33 +0700

    initial commit

---
+++ src/test/java/com/dev/reportgenerator/ReportGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.reportgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
